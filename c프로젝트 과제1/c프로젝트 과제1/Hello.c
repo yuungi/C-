43699c10ddbac663037f7f2a63395add55791005
yuungi <112606858+yuungi@users.noreply.github.com>
2022-09-09 16:00:37 +0900

    Add files via upload

---
+++ c프로젝트 과제1/c프로젝트 과제1/Hello.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int num1, num2;
+	int result1, result2, result3;
+
+	printf("Input two integers: ");
+	scanf("%d %d", &num1, &num2);
+
+	result1 = num1 & num2;
+	result2 = num1 | num2;
+	result3 = num1 ^ num2;
+
+	printf("%d & %d = %d \n", num1, num2, result1);
+	printf("%d | %d = %d \n", num1, num2, result2);
+	printf("%d ^ %d = %d \n", num1, num2, result3);
+
+	return 0;
+}
